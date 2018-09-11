500289cf8a428168d534d35761078ae33f5f2170
deam <deamme@users.noreply.github.com>
2018-09-11 22:52:11 +0200

    Added a Button component and documentation to buttons examples

---
+++ components/Button.js
@@ -0,0 +1,11 @@
+import Box from './Box'
+
+export default Box.withComponent('button').extend`
+  outline: none;
+  transition: all 0.15s ease;
+  &:hover {
+    box-shadow: 0px 2px 4px 0px rgba(72, 89, 102, 0.25);
+    transform: translateY(-4px);
+    cursor: pointer;
+  }
+`
