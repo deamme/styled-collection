c352873a61c30eeb0d9a4584b7a16e12c3267c4a
deam <deamme@users.noreply.github.com>
2018-09-13 23:21:10 +0200

    Added hoverBg and hoverColor props

--- components/Button.js
+++ components/Button.js
@@ -7,5 +7,7 @@ export default Box.withComponent('button').extend`
     box-shadow: 0px 2px 4px 0px rgba(72, 89, 102, 0.25);
     transform: translateY(-4px);
     cursor: pointer;
+    ${({ hoverBg }) => hoverBg && `background: ${hoverBg}`};
+    ${({ hoverColor }) => hoverColor && `color: ${hoverColor}`};
   }
 `
