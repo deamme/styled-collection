699329fe729ec6f278a3e82053a417570da10380
deam <deamme@users.noreply.github.com>
2018-09-14 19:46:46 +0200

    Updated Button component to only hover when it has a onClick event

--- components/Button.js
+++ components/Button.js
@@ -1,8 +1,6 @@
 import Box from './Box'
 
-export default Box.withComponent('button').extend`
-  outline: none;
-  transition: all 0.15s ease;
+const onHover = `
   &:hover {
     box-shadow: 0px 2px 4px 0px rgba(72, 89, 102, 0.25);
     transform: translateY(-4px);
@@ -10,4 +8,10 @@ export default Box.withComponent('button').extend`
     ${({ hoverBg }) => hoverBg && `background: ${hoverBg}`};
     ${({ hoverColor }) => hoverColor && `color: ${hoverColor}`};
   }
+`
+
+export default Box.withComponent('button').extend`
+  outline: none;
+  transition: all 0.15s ease;
+  ${({ onClick }) => onClick && onHover};
 `
