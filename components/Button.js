21aed5051b047b770de737757d4a65b418932e6f
deam <deamme@users.noreply.github.com>
2018-09-15 13:55:35 +0200

    Fix Button component

--- components/Button.js
+++ components/Button.js
@@ -1,17 +1,17 @@
 import Box from './Box'
 
-const onHover = `
+const onHover = (hoverBg, hoverColor) => `
   &:hover {
     box-shadow: 0px 2px 4px 0px rgba(72, 89, 102, 0.25);
     transform: translateY(-4px);
     cursor: pointer;
-    ${({ hoverBg }) => hoverBg && `background: ${hoverBg}`};
-    ${({ hoverColor }) => hoverColor && `color: ${hoverColor}`};
+    ${hoverBg && `background: ${hoverBg}`};
+    ${hoverColor && `color: ${hoverColor}`};
   }
 `
 
 export default Box.withComponent('button').extend`
   outline: none;
   transition: all 0.15s ease;
-  ${({ onClick }) => onClick && onHover};
+  ${({ onClick, hoverBg, hoverColor }) => onClick && onHover(hoverBg, hoverColor)};
 `
